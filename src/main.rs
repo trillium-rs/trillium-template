622a0db6d3346349f2ea7e39f860b464f54b93bb
Jacob Rothstein <hi@jbr.me>
2021-07-17 18:26:45 -0700

    extract out tests to src/tests.rs

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,4 @@ fn main() {
     trillium_smol::run(application())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::application;
-    use trillium_testing::prelude::*;
-
-    #[test]
-    fn says_hello_world() {
-        assert_ok!(get("/").on(&application()), "hello world!")
-    }
-
-    #[test]
-    fn says_hello_name() {
-        assert_ok!(
-            get("/hello/trillium").on(&application()),
-            "hello, trillium!"
-        );
-        assert_ok!(get("/hello/rust").on(&application()), "hello, rust!");
-    }
-
-    #[test]
-    fn other_routes_are_not_found() {
-        assert_response!(
-            get("/not/found").on(&application()),
-            404,
-            "Uh oh, I don't have a route for /not/found"
-        )
-    }
-}
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,27 @@
+#![cfg(test)]
+
+use crate::application;
+use trillium_testing::prelude::*;
+
+#[test]
+fn says_hello_world() {
+    assert_ok!(get("/").on(&application()), "hello world!")
+}
+
+#[test]
+fn says_hello_name() {
+    assert_ok!(
+        get("/hello/trillium").on(&application()),
+        "hello, trillium!"
+    );
+    assert_ok!(get("/hello/rust").on(&application()), "hello, rust!");
+}
+
+#[test]
+fn other_routes_are_not_found() {
+    assert_response!(
+        get("/not/found").on(&application()),
+        404,
+        "Uh oh, I don't have a route for /not/found"
+    )
+}
