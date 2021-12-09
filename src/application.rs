d68adc05af3ff3954ac90185839a9ceed270134c
Jacob Rothstein <hi@jbr.me>
2021-12-08 17:36:44 -0800

    update deps and add compression

--- src/application.rs
+++ src/application.rs
@@ -1,16 +1,16 @@
 use crate::routes::{not_found, router};
 use trillium::Handler;
+use trillium_compression::Compression;
 use trillium_conn_id::{log_formatter::conn_id, ConnId};
 use trillium_cookies::CookiesHandler;
 use trillium_head::Head;
-use trillium_logger::{dev_formatter, Logger, Target};
+use trillium_logger::{dev_formatter, Logger};
 use trillium_method_override::MethodOverride;
 
 pub fn application() -> impl Handler {
     (
-        Logger::new()
-            .with_target(Target::Stdout)
-            .with_formatter((dev_formatter, " ", conn_id)),
+        Logger::new().with_formatter((dev_formatter, " ", conn_id)),
+        Compression::new(),
         CookiesHandler::new(),
         Head::new(),
         MethodOverride::new(),
